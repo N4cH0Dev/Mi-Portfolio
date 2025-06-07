7ca223015faf4083737c4720e4cf35e436f3a06e
N4cH0Dev <joseignaciodiazromero.dev@gmail.com>
2025-06-07 19:31:12 -0300

    se agregaron sections

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const SectionAbout = () => {
-  return <div>SectionAbout</div>;
+  return (
+    <div className="SectionAbout">
+      <aside>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, eius
+        velit ducimus debitis necessitatibus, eligendi perferendis expedita unde
+        cum fuga placeat voluptates aliquam? Voluptatem, dicta nihil? Quaerat
+        repellendus eveniet reprehenderit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Inventore saepe accusantium iste doloribus
+        perferendis optio labore, ab mollitia, odit quod veniam voluptatum id
+        incidunt tenetur corrupti tempora, exercitationem illum necessitatibus.
+      </aside>
+      <div className="data-card">
+        <h2>Sobre mi</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
+          libero iure excepturi, dignissimos laboriosam accusamus consequatur
+          esse adipisci cupiditate totam ipsam repudiandae placeat id
+          consequuntur in reprehenderit eos voluptate ipsa! Eaque quae ducimus
+          provident suscipit, officia, laboriosam ipsam doloremque velit cum rem
+          optio? Ad aliquid perferendis voluptas, vitae, consectetur nulla,
+          aliquam modi est veniam nemo cumque laborum natus dolorem rem!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SectionAbout;
--- src/components/mains/MainHome.jsx
+++ src/components/mains/MainHome.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CardInfo from "../CardInfo";
+import SectionAbout from "../SectionAbout";
+import SectionProjects from "../SectionProjects";
 
 const MainHome = () => {
   return (
     <div className="main-container">
       <CardInfo />
+      <hr />
+      <SectionAbout />
+      <hr />
     </div>
   );
 };
